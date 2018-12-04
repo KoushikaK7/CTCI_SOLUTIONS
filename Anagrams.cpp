b4a83bbccf61141d91f146eafca94531da3fc564
KoushikaK7 <35607067+KoushikaK7@users.noreply.github.com>
2018-12-04 23:56:19 +0530

    Initial commit-Anagrams

---
+++ Anagrams.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+#include<algorithm>
+#include<string>
+#include<string.h>
+using namespace std;
+int main()
+{
+string str1,str2;
+int s1,s2;
+getline(cin,str1);
+getline(cin,str2);
+s1=sizeof(str1);
+s2=sizeof(str2);
+sort(str1,str1+s1);
+sort(str2,str2+s2);
+if(strcmp(str1,str2)==)
+	cout<<"The two words are anagrams";
+else
+	cout<<"The two words are not anagrams"<<"\n"<<str1<<"\n"<<str2;
+return 0;
+}
